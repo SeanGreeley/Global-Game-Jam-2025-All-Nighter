cb2a91dbbb430d963d5f66759775c411b9aaae9e
pppp111164730 <122899052+pppp111164730@users.noreply.github.com>
2025-01-24 21:09:55 -0600

    enemy

--- TheGame/scripts/enemy_boss.gd
+++ TheGame/scripts/enemy_boss.gd
@@ -1,17 +1,15 @@
 extends Node2D
 
 @export var player: CharacterBody2D;
-@export var nodeTexture: Texture;
+@export var parent: Node2D;#important for scoping issues
 # Called when the node enters the scene tree for the first time.
-func _enter_tree():
-	if nodeTexture != null:
-		$Enemy.sprite = nodeTexture
-	var parent = get_parent()
+func _ready():
+	parent = get_parent()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):#ignores colition, moves twoard player
-	position += global_position.direction_to(Vector2(0,0))*delta
+	parent.position += global_position.direction_to(Vector2(0,0))*delta*parent.speed
 	if player != null:
 		position += $global_position.direction_to(player.global_position)
 	#if player.global_position $global_position.x
