b151d828dc8922f42d2ea7e334b895d12511e035
SeanGreeley <robloxiansean55@gmail.com>
2025-01-25 09:57:10 -0600

    New Building Controls

--- TheGame/scripts/building_scripts/inventory_amount.gd
+++ TheGame/scripts/building_scripts/inventory_amount.gd
@@ -4,30 +4,28 @@ var wall = load("res://Scenes/building_wall.tscn")# Replace this with your actua
 var mouseButtonPressed = false # Makes sure towers aren't placed every frame the mouse is held.
 var selectWall = 0
 
+func placeWall():
+	var newWall = wall.instantiate()
+	newWall.global_transform = Global.playerPosition #will breaknwhen placed in main scene
+	newWall.rotation -= PI/2
+	newWall.position += Vector2(50*cos(newWall.rotation+PI/2),50*sin(newWall.rotation+PI/2))
+	newWall.type = selectWall
+	Global.inventory[selectWall] -= 1;
+	get_tree().root.add_child(newWall)
 
 func _input(_ev):
-	if Input.is_key_pressed(KEY_1) and Global.inventory[0] >= 1:
+	if Input.is_action_just_pressed("build1") and Global.inventory[0] > 0:
 		selectWall = 0
+		placeWall()
 	
-	if Input.is_key_pressed(KEY_2) and Global.inventory[1] >= 1:
+	if Input.is_action_just_pressed("build2") and Global.inventory[1] > 0:
 		selectWall = 1
+		placeWall()
 		
-	if Input.is_key_pressed(KEY_3) and Global.inventory[2] >= 1:
+	if Input.is_action_just_pressed("build3") and Global.inventory[2] > 0:
 		selectWall = 2
+		placeWall()
 		
-	if Input.is_key_pressed(KEY_4) and Global.inventory[3] >= 1:
+	if Input.is_action_just_pressed("build4") and Global.inventory[3] > 0:
 		selectWall = 3
-		
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-		if !mouseButtonPressed:
-			var newWall = wall.instantiate()
-			newWall.global_transform = Global.playerPosition #will breaknwhen placed in main scene
-			newWall.rotation -= PI/2
-			newWall.position += Vector2(50*cos(newWall.rotation+PI/2),50*sin(newWall.rotation+PI/2))
-			newWall.type = selectWall
-			get_tree().root.add_child(newWall)
-			mouseButtonPressed = true
-	# Set your tower variable here
-	# ex. towerScene.type = "Archer"
-	if !Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-		mouseButtonPressed = false
+		placeWall()
