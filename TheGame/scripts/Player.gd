c4d5ce185be08a33fa89d09cd0a74448161047b1
Layden Halcomb <123402670+lhalcomb@users.noreply.github.com>
2025-01-24 21:05:10 -0600

    added a way to remap buttons optimally

--- TheGame/scripts/Player.gd
+++ TheGame/scripts/Player.gd
@@ -2,10 +2,35 @@ extends CharacterBody2D
 
 const speed = 300.0
 var rotation_speed = 1.5
+
+# Function to simulate action remapping
+func handle_action_remapping():
+	if Input.is_key_pressed(KEY_W):  # W -> "ui_up"
+		Input.action_press("ui_up")
+	else:
+		Input.action_release("ui_up")
+	
+	if Input.is_key_pressed(KEY_S):  # S -> "ui_down"
+		Input.action_press("ui_down")
+	else:
+		Input.action_release("ui_down")
+	
+	if Input.is_key_pressed(KEY_A):  # A -> "ui_left"
+		Input.action_press("ui_left")
+	else:
+		Input.action_release("ui_left")
+	
+	if Input.is_key_pressed(KEY_D):  # D -> "ui_right"
+		Input.action_press("ui_right")
+	else:
+		Input.action_release("ui_right")
+
 func _physics_process(delta):
 	
 	#var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	#velocity = direction * speed
+	
+	handle_action_remapping()
 	
 	#collects input for player node
 	var move_input = Input.get_axis("ui_down", "ui_up") 
