91800f5b3803d309c48a03a4ef07d90ce9bd6868
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2025-01-25 08:59:59 -0600

    Main Menu back

---
+++ TheGame/scripts/Pause.gd
@@ -0,0 +1,30 @@
+extends Control
+
+func resume():
+	get_tree().paused = false
+	
+func pause():
+	get_tree().paused = true
+	
+func test_esc():
+	
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+		$".".show()
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:
+		$".".hide()
+		resume()
+		
+func _on_resume_pressed():
+	resume()
+	$".".hide()
+	
+func _on_main_menu_pressed():
+	resume()
+	Global.shopOpen = false
+	get_tree().change_scene_to_file("res://Scenes/Menus/Main_Menu.tscn")
+	
+
+
+func _process(delta):
+	test_esc()
