9baa81ed980d9da5145e4666f2c3383bc82b35dd
KaydenF-529781 <kbfuchs06@gmail.com>
2025-01-25 13:41:31 -0600

    just this lol

--- TheGame/scripts/building_scripts/inventory_amount.gd
+++ TheGame/scripts/building_scripts/inventory_amount.gd
@@ -4,28 +4,41 @@ var wall = load("res://Scenes/building_wall.tscn")# Replace this with your actua
 var mouseButtonPressed = false # Makes sure towers aren't placed every frame the mouse is held.
 var selectWall = 0
 
+var placePos:Vector2
+
+
+func _input(ev):
+	
+	
+	if Input.is_action_just_pressed("build1"):
+		selectWall = 0
+
+	if Input.is_action_just_pressed("build2"):
+		selectWall = 1
+		
+	if Input.is_action_just_pressed("build3"):
+		selectWall = 2
+
+		
+	if Input.is_action_just_pressed("build4"):
+		selectWall = 3
+		
+	#
+	if  ev is InputEventMouseButton and ev.button_index == MOUSE_BUTTON_RIGHT and Global.inventory[selectWall] > 0:
+		var camera = get_viewport().get_camera_2d()
+		placePos = camera.get_global_mouse_position()
+		placeWall()
+		
+		
+	
+
 func placeWall():
 	var newWall = wall.instantiate()
-	newWall.global_transform = Global.playerPosition #will breaknwhen placed in main scene
-	newWall.rotation -= PI/2
-	newWall.position += Vector2(50*cos(newWall.rotation+PI/2),50*sin(newWall.rotation+PI/2))
+	#newWall.global_transform = Global.playerPosition #will breaknwhen placed in main scene
+	#newWall.rotation -= PI/2
+	#newWall.position += Vector2(50*cos(newWall.rotation+PI/2),50*sin(newWall.rotation+PI/2))
+	newWall.position = placePos
+	
 	newWall.type = selectWall
 	Global.inventory[selectWall] -= 1;
 	get_tree().root.add_child(newWall)
-
-func _input(_ev):
-	if Input.is_action_just_pressed("build1") and Global.inventory[0] > 0:
-		selectWall = 0
-		placeWall()
-	
-	if Input.is_action_just_pressed("build2") and Global.inventory[1] > 0:
-		selectWall = 1
-		placeWall()
-		
-	if Input.is_action_just_pressed("build3") and Global.inventory[2] > 0:
-		selectWall = 2
-		placeWall()
-		
-	if Input.is_action_just_pressed("build4") and Global.inventory[3] > 0:
-		selectWall = 3
-		placeWall()
