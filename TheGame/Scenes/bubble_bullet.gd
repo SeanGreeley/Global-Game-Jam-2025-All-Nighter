187730f424b9f001371f118ce69a445848974255
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2025-01-25 09:08:49 -0600

    Poision bubbles

--- TheGame/Scenes/bubble_bullet.gd
+++ TheGame/Scenes/bubble_bullet.gd
@@ -5,7 +5,10 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	health = 1
+	if Global.hasAcid:
+		health = 3
+	else:
+		health = 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
