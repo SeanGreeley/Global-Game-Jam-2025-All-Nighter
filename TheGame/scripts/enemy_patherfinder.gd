bc5e7c2e5c7de8889d057e14697924937187fb2e
SeanGreeley <robloxiansean55@gmail.com>
2025-01-25 00:51:18 -0600

    fixed red error

--- TheGame/scripts/enemy_patherfinder.gd
+++ TheGame/scripts/enemy_patherfinder.gd
@@ -4,15 +4,18 @@ extends Area2D
 var finished = false
 var parent: Node2D;
 
+var loaded = false;
+
 func _ready():
 	nav.target_position = Vector2(100,100)
 	parent = get_parent()
 
 func _physics_process(delta):
-	if not finished:
+	if not finished and loaded:
 		var dir = (nav.get_next_path_position() - global_position).normalized()
 		parent.translate(dir * parent.speed * delta)
-
+	if not loaded:
+		loaded = true
 #func _process(delta):
 	#nav.target_position = #PlyerPos
 
