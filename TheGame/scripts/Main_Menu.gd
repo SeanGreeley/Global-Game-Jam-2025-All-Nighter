d85d93a9d533b2c9179955cc19a1fa15c5064b99
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2025-01-25 09:52:28 -0600

    Main Menu Music

--- TheGame/scripts/Main_Menu.gd
+++ TheGame/scripts/Main_Menu.gd
@@ -5,15 +5,13 @@ extends Control
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	if $AudioListener2D.playing == false:
+		$AudioListener2D.playing = true
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
-	
 	
 func _on_quit_pressed():
 	get_tree().quit()
