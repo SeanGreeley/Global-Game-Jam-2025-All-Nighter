56571127a99e76ae6ba564d863ed03a3a45e6668
KaydenF-529781 <kbfuchs06@gmail.com>
2025-01-25 04:48:23 -0600

    wall go brrrrrrrr

--- TheGame/global.gd
+++ TheGame/global.gd
@@ -4,7 +4,7 @@ var bubbleHealth:int = 100
 var bossHealth:int
 var roundCounter:int = 1
 var coinCounter:int = 0
-var inventory:Array = [4]
+var inventory:Array = [0,0,0,0]
 var stunned:bool = false
 var shopOpen:bool = false
 var playerPosition: Vector2;
--- TheGame/scripts/building_scripts/inventory_amount.gd
+++ TheGame/scripts/building_scripts/inventory_amount.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-var wall = load("res://Scenes/building_coral.tscn")# Replace this with your actual tower scene
+var wall = load("res://Scenes/building_wall.tscn")# Replace this with your actual tower scene
 var mouseButtonPressed = false # Makes sure towers aren't placed every frame the mouse is held.
 var selectWall = 0
+
 
 func _input(_ev):
 	if Input.is_key_pressed(KEY_1) and Global.inventory[0] >= 1:
@@ -14,10 +15,10 @@ func _input(_ev):
 	if Input.is_key_pressed(KEY_3) and Global.inventory[2] >= 1:
 		selectWall = 2
 		
-	if Input.is_key_pressed(KEY_3) and Global.inventory[3] >= 1:
+	if Input.is_key_pressed(KEY_4) and Global.inventory[3] >= 1:
 		selectWall = 3
 		
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and Global.inventory[0] != 0 and Global.inventory[1] != 0 and Global.inventory[2] != 0 and Global.inventory[3] != 0:
 		if !mouseButtonPressed:
 			var newWall = wall.instantiate()
 			newWall.position = get_viewport().get_mouse_position() - Vector2(576,324) #will breaknwhen placed in main scene
