9f8a04874a4ec3dae4b144c8c58b7b426b8d9934
KaydenF-529781 <kbfuchs06@gmail.com>
2025-01-25 00:48:12 -0600

    jimbash

--- TheGame/scripts/building_scripts/building.gd
+++ TheGame/scripts/building_scripts/building.gd
@@ -1,13 +1,20 @@
 extends Node
 
 var health = 100
-
+var type = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimatedSprite2D.frame = type
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if type == 0:
+		health = 500
+	elif type == 1:
+		health = 250
+	else:
+		health = 100
+	
 	if health == 0:
 		queue_free()
