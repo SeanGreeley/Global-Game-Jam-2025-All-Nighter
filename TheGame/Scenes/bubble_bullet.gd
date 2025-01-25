afefd8397317d2283d860a598016050f5bf297c0
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2025-01-24 21:45:27 -0600

    Bullet Health

--- TheGame/Scenes/bubble_bullet.gd
+++ TheGame/Scenes/bubble_bullet.gd
@@ -1,10 +1,11 @@
 extends RigidBody2D
 
 @export var isBullet:bool = true
+@export var health:int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	health = 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
