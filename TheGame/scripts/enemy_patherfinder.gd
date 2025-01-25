90d7219f4d7ec1674f274144b58fab9053b624c7
KaydenF-529781 <kbfuchs06@gmail.com>
2025-01-25 01:50:35 -0600

    Update enemy_patherfinder.gd

--- TheGame/scripts/enemy_patherfinder.gd
+++ TheGame/scripts/enemy_patherfinder.gd
@@ -4,14 +4,21 @@ extends Area2D
 var finished = false
 var parent: Node2D;
 
+var loaded = false;
+
 func _ready():
 	nav.target_position = Vector2(100,100)
 	parent = get_parent()
 
 func _physics_process(delta):
-	if not finished:
+	if not finished and loaded:
 		var dir = (nav.get_next_path_position() - global_position).normalized()
 		parent.translate(dir * parent.speed * delta)
+<<<<<<< HEAD
+=======
+	if not loaded:
+		loaded = true
+>>>>>>> 938a2ab8aa5bd8c7cba641448dc9e25e5a10ef1d
 #func _process(delta):
 	#nav.target_position = #PlyerPos
 
