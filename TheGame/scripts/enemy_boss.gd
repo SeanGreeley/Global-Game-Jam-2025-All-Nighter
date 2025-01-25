20040a4dbd32c34b512c701ae7e94ec749389b30
pppp111164730 <122899052+pppp111164730@users.noreply.github.com>
2025-01-24 22:55:01 -0600

    enemy

--- TheGame/scripts/enemy_boss.gd
+++ TheGame/scripts/enemy_boss.gd
@@ -2,14 +2,21 @@ extends Node2D
 
 @export var player: CharacterBody2D;
 @export var parent: Node2D;#important for scoping issues
+var nottarget = Vector2(0,0)
+var target: Vector2;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	parent = get_parent()
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):#ignores colition, moves twoard player
-	parent.position += global_position.direction_to(Vector2(0,0))*delta*parent.speed
 	if player != null:
-		position += $global_position.direction_to(player.global_position)
-	#if player.global_position $global_position.x
+		if target != Vector2(0,0):
+			target = player.position;
+		if position.distance_to(nottarget) < position.distance_to(target) * 0.8:
+			var temp = target
+			target = nottarget
+			nottarget = temp
+		parent.position += $global_position.direction_to(target)*delta*parent.speed
+	elif player == null:
+		parent.position += global_position.direction_to(Vector2(0,0))*delta*parent.speed
--- TheGame/scripts/enemy_patherfinder.gd
+++ TheGame/scripts/enemy_patherfinder.gd
@@ -2,14 +2,16 @@ extends Area2D
 
 @onready var nav : NavigationAgent2D = $NavigationAgent2D
 var finished = false
+var parent: Node2D;
 
 func _ready():
 	nav.target_position = Vector2(100,100)
+	parent = get_parent()
 
 func _physics_process(delta):
 	if not finished:
 		var dir = (nav.get_next_path_position() - global_position).normalized()
-		translate(dir * 200 * delta)
+		parent.translate(dir * parent.speed * delta)
 
 #func _process(delta):
 	#nav.target_position = #PlyerPos
