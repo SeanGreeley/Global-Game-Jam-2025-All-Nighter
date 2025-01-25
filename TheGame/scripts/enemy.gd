7d7d112b1d0a5793b7d34fd15ecc0371ed74992f
pppp111164730 <122899052+pppp111164730@users.noreply.github.com>
2025-01-24 21:40:17 -0600

    signals

--- TheGame/scripts/enemy.gd
+++ TheGame/scripts/enemy.gd
@@ -20,3 +20,7 @@ func hit(damage: int):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_area_2d_body_entered(body):
+	pass # Replace with function body.
